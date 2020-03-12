ad34e57e89bddf6dbf09c4796c34a25ae687c1bc
buckleyderekp <buckleyderekp@gmail.com>
2020-03-12 14:55:05 -0500

    added event to font buttons

--- scripts/theme/FontButtons.js
+++ scripts/theme/FontButtons.js
@@ -1,3 +1,29 @@
+
+
+const eventHub = document.querySelector("#container")
+
+eventHub.addEventListener("click", clickEvent => {
+
+    // Make sure it was one of the font buttons
+    if (clickEvent.target.id.startsWith("btnFont--")) {
+
+        // Get the chosen font
+        const [prefix, chosenSize] = clickEvent.target.id.split("--")
+
+        /*
+            Create a new custom event, with a good name, and
+            add a property to the `detail` object that specifies
+            which color was chosen
+        */
+        const fontChosenEvent = new CustomEvent("fontChosen", {
+            detail: {
+                fontSize: chosenSize
+            }
+        })
+
+        eventHub.dispatchEvent(fontChosenEvent)
+    }
+})
 export const FontButtons = () => {
     return `
         <article class="fonts">
