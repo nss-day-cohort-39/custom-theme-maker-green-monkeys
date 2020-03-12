6a335747988f38581266fb0d2384d29ceee6284b
ianpenn08 <inkmpenn@gmail.com>
2020-03-12 15:13:38 -0500

    intial commit

--- scripts/theme/BorderButtons.js
+++ scripts/theme/BorderButtons.js
@@ -1,3 +1,30 @@
+const eventHub = document.querySelector(".container")
+const contentTarget = document.querySelector(".themes")
+
+// Listen for browser generated click event in this component
+contentTarget.addEventListener("click", clickEvent => {
+
+    // Make sure it was one of the color buttons
+    if (clickEvent.target.id.startsWith("pixelsize")) {
+
+        // Get the chosen color
+        const [chosenPixel] = clickEvent.target.id.split("--")
+
+        /*
+            Create a new custom event, with a good name, and
+            add a property to the `detail` object that specifies
+            which color was chosen
+        */
+        const pixelChosenEvent = new CustomEvent("pixelChosen", {
+            detail: {
+                pixel: chosenPixel
+            }
+        })
+
+        eventHub.dispatchEvent(pixelChosenEvent)
+    }
+})
+
 export const BorderButtons = () => {
     return `
         <article class="borderSizes">
