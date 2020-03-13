40744fadfb4777c2901af5c8291b2930aa3218fe
buckleyderekp <buckleyderekp@gmail.com>
2020-03-13 14:47:29 -0500

    added all events to news

--- scripts/news/NewsList.js
+++ scripts/news/NewsList.js
@@ -18,8 +18,64 @@ const render = newsCollection => {
 
 eventHub.addEventListener("fontChosen", event => {
     const font = event.detail.fontSize
+
+    const contentTarget = document.querySelector('.news')
+
+    if (contentTarget.classList.contains("xsmall")) {
+       contentTarget.classList.remove("xsmall")
+    }
+    else if (contentTarget.classList.contains("small")){
+        contentTarget.classList.remove("small")
+    }
+    else if (contentTarget.classList.contains("large")){
+        contentTarget.classList.remove("large")
+    }
+    else if (contentTarget.classList.contains("xlarge")){
+        contentTarget.classList.remove("xlarge")
+    }
+
+    // contentTarget.classList = []
+    contentTarget.classList.add(font)
+})
+
+eventHub.addEventListener("colorChosen", event => {
+
+    const color = event.detail.color
+
+    const contentTarget = document.querySelector(".news")
+
+    if (contentTarget.classList.contains("blue")) {
+        contentTarget.classList.remove("blue")
+     }
+     else if (contentTarget.classList.contains("green")){
+         contentTarget.classList.remove("green")
+     }
+     else if (contentTarget.classList.contains("purple")){
+         contentTarget.classList.remove("purple")
+     }
+     else if (contentTarget.classList.contains("red")){
+         contentTarget.classList.remove("red")
+     }
+
+    // contentTarget.classList = []
+    contentTarget.classList.add(color)
+
+})
+
+eventHub.addEventListener("pixelChosen", event => {
+    const score = event.detail.pixel
     
     const contentTarget = document.querySelector('.news')
-    contentTarget.classList = []
-    contentTarget.classList.add(font, "container__panel", "news")
-})
+
+    if (contentTarget.classList.contains("onepixel")) {
+        contentTarget.classList.remove("onepixel")
+     }
+     else if (contentTarget.classList.contains("threepixels")){
+         contentTarget.classList.remove("threepixels")
+     }
+     else if (contentTarget.classList.contains("fivepixels")){
+         contentTarget.classList.remove("fivepixels")
+     }
+    // contentTarget.classList = []
+    contentTarget.classList.add(score)
+    })
