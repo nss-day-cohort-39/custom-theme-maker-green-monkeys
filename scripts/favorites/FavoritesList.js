e5db7c5a0a55890e7f3ff526199dad3084f89cbc
Thomas Long <thomas@Thomass-MacBook-Pro.local>
2020-03-13 15:39:48 -0500

    got everything working together

--- scripts/favorites/FavoritesList.js
+++ scripts/favorites/FavoritesList.js
@@ -17,39 +17,66 @@ const render = favoriteCollection => {
     `
 }
 
+eventHub.addEventListener("fontChosen", event => {
+    const font = event.detail.fontSize
+
+    const contentTarget = document.querySelector('.favorites')
+
+    if (contentTarget.classList.contains("xsmall")) {
+       contentTarget.classList.remove("xsmall")
+    }
+    else if (contentTarget.classList.contains("small")){
+        contentTarget.classList.remove("small")
+    }
+    else if (contentTarget.classList.contains("large")){
+        contentTarget.classList.remove("large")
+    }
+    else if (contentTarget.classList.contains("xlarge")){
+        contentTarget.classList.remove("xlarge")
+    }
+
+    // contentTarget.classList = []
+    contentTarget.classList.add(font)
+})
+
 eventHub.addEventListener("colorChosen", event => {
 
     const color = event.detail.color
 
     const contentTarget = document.querySelector(".favorites")
-    contentTarget.classList = []
-    contentTarget.classList.add(color, "container__panel", "favorites")
+
+    if (contentTarget.classList.contains("blue")) {
+        contentTarget.classList.remove("blue")
+     }
+     else if (contentTarget.classList.contains("green")){
+         contentTarget.classList.remove("green")
+     }
+     else if (contentTarget.classList.contains("purple")){
+         contentTarget.classList.remove("purple")
+     }
+     else if (contentTarget.classList.contains("red")){
+         contentTarget.classList.remove("red")
+     }
+
+    // contentTarget.classList = []
+    contentTarget.classList.add(color)
 
 })
 
-// const eventHub = document.querySelector("#container")
-
-// export const NewsList = () => {
-//     const newsItems = useNews()
-//     return render(newsItems)
-// }
-
-// const render = newsCollection => {
-//     return `
-//     <article class="container__panel news">
-//     ${newsCollection.map(news => NewsItem(news)).join("")}
-//     </article>
-//     `
-// }
-
-// eventHub.addEventListener("fontChosen", event => {
-//     const font = event.detail.fontSize
+eventHub.addEventListener("pixelChosen", event => {
+    const score = event.detail.pixel
     
-//     const contentTarget = document.querySelector('.news')
-//     contentTarget.classList = []
-//     contentTarget.classList.add(font, "container__panel", "news")
-// })
+    const contentTarget = document.querySelector('.favorites')
 
-
-
-
+    if (contentTarget.classList.contains("onepixel")) {
+        contentTarget.classList.remove("onepixel")
+     }
+     else if (contentTarget.classList.contains("threepixels")){
+         contentTarget.classList.remove("threepixels")
+     }
+     else if (contentTarget.classList.contains("fivepixels")){
+         contentTarget.classList.remove("fivepixels")
+     }
+    // contentTarget.classList = []
+    contentTarget.classList.add(score)
+    })
